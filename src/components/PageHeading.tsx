b73a10005db780b6f8268ed5d9e6cbb84d255d7e
Steph254 <steph.mwikali@gmail.com>
2022-05-25 14:37:00 +0300

    Initial commit

---
+++ src/components/PageHeading.tsx
@@ -0,0 +1,13 @@
+import {ReactNode} from "react"
+interface Props {
+    extraClasses: string
+    children: ReactNode
+  }
+  
+  const PageHeading: React.FC<Props> = ({ children, extraClasses }) => {
+    const classes = "text-4xl text-green-900 font-semibold " + extraClasses
+  
+    return <h1 className={classes}>{children}</h1>
+  }
+  
+  export default PageHeading
